aa7f1bb76cacbb3e9f39de44c78db2c0a5b41370
jwilliampope <jwilliampope@hotmail.com>
2021-02-13 16:53:55 -0500

    committed all files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let search = document.getElementById("searchInput").addEventListener("keyup", event => {
+  let searchQuery = event.target.value.toLowerCase()
+  let allNamesDOMCollection = document.getElementsByClassName("name")
+
+  for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+    const currentName = allNamesDOMCollection[counter].textContent.toLowerCase()
+
+    if (currentName.includes(searchQuery)) {
+      allNamesDOMCollection[counter].style.display = "block"
+    } else {
+      allNamesDOMCollection[counter].style.display = "none"
+    }
+  }
+})
